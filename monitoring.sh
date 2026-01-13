b6b7a07aa5868adaf55d697e55505c64835da27a
Sibin das S <sibind961@gmail.com>
2026-01-13 12:05:32 +0530

    monitoring

---
+++ monitoring.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+LOGFILE="$HOME/linux-automation-toolkit/logs/monitor.log"
+
+CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}')
+MEM=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+DISK=$(df / | tail -1 | awk '{print $5}')
+
+echo "$(date) CPU:$CPU% MEM:$MEM% DISK:$DISK" >> "$LOGFILE"
